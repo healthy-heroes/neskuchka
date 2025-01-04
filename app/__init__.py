e983c9920107e241067c4225560bf1c6145bddf9
dzlk <dlb.diesel@gmail.com>
2025-01-04 18:14:55 +0500

    Add hurl and jq

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,22 @@ app = FastAPI()
 @app.get("/api/ping")
 async def root():
     return {"message": "Pong"}
+
+
+exercises = [
+    {"id": 1, "name": "Push-ups"},
+    {"id": 2, "name": "Pull-ups"},
+    {"id": 3, "name": "Squats"},
+]
+
+@app.get("/api/exersices")
+async def get_exersices():
+    return exercises
+
+
+@app.get("/api/exersices/{id}")
+async def get_exersice(id: int):
+    exercise = next((e for e in exercises if e["id"] == id), None)
+    return {"data": exercise}
+
+
