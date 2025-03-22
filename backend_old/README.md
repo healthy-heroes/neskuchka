8e16ad6e7480f74de64fb9781f12ba0bba7290a3
dzlk <dlb.diesel@gmail.com>
2025-03-22 21:49:56 +0500

    Remove python back

--- backend_old/README.md
+++
@@ -1,2 +0,0 @@
-# Neskuchka backend
-
--- backend_old/app/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_info(client: TestClient):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"name": "Test app", "environment": "test"}
