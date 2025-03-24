1ea0cac79db887aa5d414cd5362ff323e62899d3
dzlk <dlb.diesel@gmail.com>
2025-03-24 09:33:07 +0500

    Introduce model for db

--- backend/app/store/datastore/workout.go
+++ backend/app/store/datastore/workout.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"encoding/json"
 	"errors"
+	"time"
 
 	"github.com/rs/zerolog/log"
 
@@ -13,16 +14,55 @@ type WorkoutDBStore struct {
 	*DataStore
 }
 
-func (ds *WorkoutDBStore) Create(workout *store.Workout) (*store.Workout, error) {
+type WorkoutDB struct {
+	ID       string
+	Date     time.Time
+	TrackID  string `db:"track_id"`
+	Sections []byte
+}
+
+func workoutDBFromStore(workout *store.Workout) (*WorkoutDB, error) {
 	// Serialize sections to JSON
 	sectionsJSON, err := json.Marshal(workout.Sections)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to serialize sections to JSON")
+		return nil, err
+	}
+
+	return &WorkoutDB{
+		ID:       string(workout.ID),
+		Date:     workout.Date,
+		TrackID:  string(workout.TrackID),
+		Sections: sectionsJSON,
+	}, nil
+}
+
+func (w *WorkoutDB) toStore() (*store.Workout, error) {
+	workout := &store.Workout{
+		ID:      store.WorkoutID(w.ID),
+		Date:    w.Date,
+		TrackID: store.TrackID(w.TrackID),
+	}
+
+	// Deserialize sections from JSON
+	err := json.Unmarshal(w.Sections, &workout.Sections)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to deserialize sections from JSON")
+		return nil, err
+	}
+
+	return workout, nil
+}
+
+func (ds *WorkoutDBStore) Create(workout *store.Workout) (*store.Workout, error) {
+	dbWorkout, err := workoutDBFromStore(workout)
 	if err != nil {
 		return nil, err
 	}
 
 	_, err = ds.DB.Exec(`INSERT INTO workout (id, date, track_id, sections) 
 						VALUES (?, ?, ?, ?)`,
-		workout.ID, workout.Date, workout.TrackID, sectionsJSON)
+		dbWorkout.ID, dbWorkout.Date, dbWorkout.TrackID, dbWorkout.Sections)
 	if err != nil {
 		return nil, err
 	}
@@ -30,88 +70,55 @@ func (ds *WorkoutDBStore) Create(workout *store.Workout) (*store.Workout, error)
 }
 
 func (ds *WorkoutDBStore) Get(id store.WorkoutID) (*store.Workout, error) {
-	var workout store.Workout
-	var sectionsJSON []byte
+	var dbWorkout WorkoutDB
 
-	err := ds.DB.QueryRow(`SELECT id, date, track_id, sections FROM workout WHERE id = ?`,
-		id).Scan(&workout.ID, &workout.Date, &workout.TrackID, &sectionsJSON)
+	err := ds.DB.Get(&dbWorkout, `SELECT id, date, track_id, sections FROM workout WHERE id = ?`, id)
 	if err != nil {
 		return nil, err
 	}
 
-	// Deserialize sections from JSON
-	err = json.Unmarshal(sectionsJSON, &workout.Sections)
+	workout, err := dbWorkout.toStore()
 	if err != nil {
-		log.Error().Err(err).Msg("Failed to deserialize sections from JSON")
 		return nil, err
 	}
 
-	log.Debug().Msgf("workout: %+v", workout)
-
-	return &workout, nil
+	return workout, nil
 }
 
 func (ds *WorkoutDBStore) Find(criteria *store.WorkoutFindCriteria) ([]*store.Workout, error) {
 	if criteria.TrackID == "" {
-		return nil, errors.New("track_id is required")
+		return nil, errors.New("criteria.TrackID is required")
 	}
 
-	var (
-		workouts []*store.Workout
-		args     []interface{}
-		query    string
-	)
+	query := `SELECT id, date, track_id, sections FROM workout WHERE track_id = ? ORDER BY date DESC LIMIT ?`
 
-	// Build query based on criteria
-	query = `
-		SELECT id, date, track_id, sections 
-		FROM workout WHERE track_id = ? ORDER BY date DESC LIMIT ?`
-	args = append(args, criteria.TrackID)
-
-	// Apply limit
 	limit := criteria.Limit
 	if limit <= 0 {
-		limit = 10 // Default limit
+		limit = 50
 	}
-	args = append(args, limit)
 
-	// Execute query
-	rows, err := ds.DB.Query(query, args...)
+	dbWorkouts := make([]*WorkoutDB, 0)
+	err := ds.DB.Select(&dbWorkouts, query, criteria.TrackID, limit)
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
 
-	// Process results
-	for rows.Next() {
-		var workout store.Workout
-		var sectionsJSON []byte
-
-		err := rows.Scan(&workout.ID, &workout.Date, &workout.TrackID, &sectionsJSON)
+	workouts := make([]*store.Workout, 0)
+	for _, dbWorkout := range dbWorkouts {
+		workout, err := dbWorkout.toStore()
 		if err != nil {
 			return nil, err
 		}
-
-		// Deserialize sections from JSON
-		err = json.Unmarshal(sectionsJSON, &workout.Sections)
-		if err != nil {
-			return nil, err
-		}
-
-		workouts = append(workouts, &workout)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
+		workouts = append(workouts, workout)
 	}
 
 	return workouts, nil
 }
 
+// InitTables initializes the workout table
 func (ds *WorkoutDBStore) InitTables() error {
 	log.Debug().Msg("Creating workout table")
 
-	// Create workout table
 	_, err := ds.DB.Exec(`
 		CREATE TABLE IF NOT EXISTS workout (
 			id TEXT PRIMARY KEY NOT NULL,
