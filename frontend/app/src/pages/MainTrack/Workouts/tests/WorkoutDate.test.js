bcdbd5e6e362d9f1669cabd51ef0417ef15ff6af
dzlk <dlb.diesel@gmail.com>
2025-04-17 09:33:06 +0500

    Add worklflow for frontend

--- frontend/app/src/pages/MainTrack/Workouts/tests/WorkoutDate.test.js
+++ frontend/app/src/pages/MainTrack/Workouts/tests/WorkoutDate.test.js
@@ -1,17 +1,17 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { WorkoutDate } from '../WorkoutDate'
+import { WorkoutDate } from "../WorkoutDate";
 
-describe('WorkoutDate', () => {
-    it('should return the correct date without year if it is current year', () => {
-        const date = `${new Date().getFullYear()}-03-04`
-        const formattedDate = WorkoutDate(date)
-        expect(formattedDate).toBe('4 марта')
-    })
+describe("WorkoutDate", () => {
+  it("should return the correct date without year if it is current year", () => {
+    const date = `${new Date().getFullYear()}-03-04`;
+    const formattedDate = WorkoutDate(date);
+    expect(formattedDate).toBe("4 марта");
+  });
 
-    it('should return the correct date with year', () => {
-        const date = `${new Date().getFullYear() - 2}-02-28`
-        const formattedDate = WorkoutDate(date)
-        expect(formattedDate).toBe('28 февраля 2023')
-    })
-})
+  it("should return the correct date with year", () => {
+    const date = `${new Date().getFullYear() - 2}-02-28`;
+    const formattedDate = WorkoutDate(date);
+    expect(formattedDate).toBe("28 февраля 2023");
+  });
+});
