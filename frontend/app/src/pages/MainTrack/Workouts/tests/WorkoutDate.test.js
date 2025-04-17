3fa657c1d7e3c2dcefe83a60924c1588cda63ac1
dzlk <dlb.diesel@gmail.com>
2025-04-17 08:36:48 +0500

    Add frontend test and coverage

---
+++ frontend/app/src/pages/MainTrack/Workouts/tests/WorkoutDate.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest'
+
+import { WorkoutDate } from '../WorkoutDate'
+
+describe('WorkoutDate', () => {
+    it('should return the correct date without year if it is current year', () => {
+        const date = `${new Date().getFullYear()}-03-04`
+        const formattedDate = WorkoutDate(date)
+        expect(formattedDate).toBe('4 марта')
+    })
+
+    it('should return the correct date with year', () => {
+        const date = `${new Date().getFullYear() - 2}-02-28`
+        const formattedDate = WorkoutDate(date)
+        expect(formattedDate).toBe('28 февраля 2023')
+    })
+})
