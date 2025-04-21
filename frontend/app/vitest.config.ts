a690e52ee6ff8e34f2d8d973cf6421d8da55646b
dzlk <dlb.diesel@gmail.com>
2025-04-21 20:56:08 +0500

    Add Storybook

--- frontend/app/vitest.config.ts
+++ frontend/app/vitest.config.ts
@@ -1,9 +1,67 @@
-import { defineConfig } from "vitest/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({
-  test: {
-    coverage: {
-      reporter: ["text", "json", "html", "lcov"],
+import { mergeConfig, defineConfig } from "vitest/config";
+import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+
+import viteConfig from "./vite.config";
+
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      pool: "threads",
+
+      coverage: {
+        provider: "v8",
+        reporter: ["text", "json", "html", "lcov"],
+        reportsDirectory: "./coverage",
+      },
+
+      workspace: [
+        {
+          extends: true,
+
+          plugins: [
+            // The plugin will run tests for the stories defined in your Storybook config
+            // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+            storybookTest({ configDir: path.join(dirname, ".storybook") }),
+          ],
+
+          test: {
+            name: "stories",
+            include: ["src/**/*.stories.ts"],
+
+            browser: {
+              enabled: true,
+              provider: "playwright",
+              instances: [{ browser: "chromium" }],
+              headless: true,
+            },
+
+            setupFiles: [".storybook/vitest.setup.ts"],
+          },
+        },
+        {
+          extends: true,
+
+          test: {
+            environment: "node",
+            name: "unit",
+            include: ["src/**/*.test.ts"],
+          },
+        },
+      ],
+
+      // environmentMatchGlobs: [
+      //   ["src/**/*.stories.ts", "jsdom"],
+      //   ["src/**/*.test.ts", "node"],
+      // ],
     },
-  },
-});
+  }),
+);
