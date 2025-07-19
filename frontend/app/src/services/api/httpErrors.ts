643ec8f151cead0247e9d79f2b74366b53236e2d
dzlk <dlb.diesel@gmail.com>
2025-07-19 17:22:39 +0500

    Remove old frontend. Bye

--- frontend/app/src/services/api/httpErrors.ts
+++
@@ -1,8 +0,0 @@
-export class HttpError extends Error {
-  constructor(
-    public status: number,
-    public details: object,
-  ) {
-    super(JSON.stringify(details));
-  }
-}
--- frontend/app/src/services/api/methods.mock.ts
+++
@@ -1,7 +0,0 @@
-import { fn } from "@storybook/test";
-import * as actual from "./methods";
-
-export * from "./methods";
-export const getMainTrackWorkouts = fn(actual.getMainTrackWorkouts).mockName(
-  "getMainTrackWorkouts",
-);
