1b4c4a47e3409d1b7b43650bdd546c64be097dee
dzlk <dlb.diesel@gmail.com>
2026-01-29 12:02:41 +0500

    Make logo clickable and add formatting

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import { Text } from '@mantine/core';
+import { RouteLink } from '../RouteLink/RouteLink';
 import { LogoIcon } from './LogoIcon';
 import classes from './Logo.module.css';
 
 export function Logo() {
 	return (
-		<Text size="xl" fw={700}>
-			<LogoIcon className={classes.logoIcon} size="xl" />
-			&nbsp;Neskuchka
-		</Text>
+		<RouteLink to="/" underline="never" c="black">
+			<Text size="xl" fw={700}>
+				<LogoIcon className={classes.logoIcon} size="xl" />
+				&nbsp;Neskuchka
+			</Text>
+		</RouteLink>
 	);
 }
